f291c0fd266ceeddc3a123c971291b097658b335
Ngoc Tuan <tranngoctuan20102002@gmail.com>
2023-03-01 22:46:25 +0700

    Revert "Add Color Model and ProductVariant Models"
    This reverts commit 1b36194fb8857fb0635e0a3293a3ba01ea304f15.

--- home_page/models.py
+++ home_page/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     price = models.FloatField(default=0.0)
     image = models.ImageField(upload_to='products/%Y/%m', default=None)
     active = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, related_name='product', on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(Category,related_name='category', on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.name
@@ -45,7 +45,6 @@ class Order(models.Model):
     orderDate = models.DateField(auto_now_add=True)
     orderStatus = models.BooleanField(default=False)
     orderAddress = models.CharField(max_length=200)
-    products = models.ManyToManyField(Product, related_name='order', through='OrderDetail')
     user = models.ForeignKey(User, related_name='order', on_delete=models.CASCADE)
 
 
@@ -55,20 +54,3 @@ class OrderDetail(models.Model):
     product = models.ForeignKey(Product, related_name='order_detail', on_delete=models.DO_NOTHING)
     order = models.ForeignKey(Order, related_name='order_detail', on_delete=models.DO_NOTHING)
 
-class Color(models.Model):
-    title = models.CharField(max_length=100)
-    color_code = models.CharField(max_length=50, null=True)
-    def __str__(self):
-        return self.title
-
-class ProductVariant(models.Model):
-    product = models.ForeignKey(Product, related_name='variant', on_delete=models.CASCADE)
-    color = models.ForeignKey(Color, related_name='variant', on_delete=models.CASCADE, blank=True, null=True)
-    amount_in_stock = models.IntegerField()
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['product', 'color'],
-                name='unique_prod_color_size_combo'
-            )
-        ]
