ba078ccc5b65f4a155260e8710d8470871cdc0eb
2051052029duc@ou.edu.vn <2051052029duc@ou.edu.vn>
2023-03-11 01:13:09 +0700

    update cart

--- home_page/template_context.py
+++ home_page/template_context.py
@@ -1,5 +1,5 @@
 from home_page.models import Category
-
+from django.http import JsonResponse
 
 def get_category(request):
     categories = Category.objects.all()
@@ -7,3 +7,28 @@ def get_category(request):
         'categories': categories,
     }
     return data
+
+def add_To_Cart(request):
+    if 'cart' not in request.session:
+        request.session['cart'] = {}
+    cart = request.session['cart']
+    id = request.POST.get('id')
+    name = request.POST.get('name')
+    price = request.POST.get('price')
+    image = request.POST.get('image')
+    quantity = request.POST.get('quantity')
+    if id in cart:
+        cart[id]['quantity'] = int(cart[id]['quantity']) + 1
+    else:
+        cart[id] = {
+            'id': id,
+            'name': name,
+            'price': price,
+            'image': image,
+            'quantity': quantity
+        }
+    request.session['cart'] = cart
+    cartInfo = request.session['cart']
+    # html = render_to_string('cart.html', {'cart': cartInfo})
+    # return HttpResponse('cart.html', {'cart': cartInfo})
+    return JsonResponse({'cart': request.session['cart'], 'totalitems':len(request.session['cart'])})
