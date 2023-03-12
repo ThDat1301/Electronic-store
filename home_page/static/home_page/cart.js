7eef0a8735163713bba21bd02d14409a2b298461
2051052029duc@ou.edu.vn <2051052029duc@ou.edu.vn>
2023-03-12 22:51:06 +0700

    update cart 3

--- home_page/static/home_page/cart.js
+++ home_page/static/home_page/cart.js
@@ -1,4 +1,5 @@
 $(document).ready(function (){
+    // add to cart
     $(".addToCartBtn").on('click', function () {
         var _vm=$(this);
         var _qty = $("#quantity").val()
@@ -18,13 +19,52 @@ $(document).ready(function (){
                 'quantity': _qty
             },
             dataType: 'json',
-            beforeSend:function(){
-				_vm.attr('disabled',true);
-			},
-			success:function(res){
-				$(".cart-list").text(res.totalitems);
-				_vm.attr('disabled',false);
-			}
+            complete: function () {
+                location.reload()
+            }
+        })
+    })
+
+    //delete item from cart
+    $(".btn-delete").on('click', function () {
+        var _pId = $(this).attr('data-item')
+        $.ajax({
+            type: "POST",
+            url: "/delete-from-cart",
+            data: {
+                'id': _pId,
+            },
+            dataType: "json",
+            complete: function () {
+                location.reload()
+            }
+        })
+    })
+
+    //reduce quantity item
+    $(".reduceFromCartBtn").on('click', function () {
+        var _vm=$(this);
+        var _qty = $("#quantity").val()
+        if (!_qty)
+            _qty = 1
+        var _productId = $(this).attr("data-id")
+        var _productTitle = $(this).attr("data-title")
+        var _productPrice = $(this).attr("data-price")
+        var _productImage = $(this).attr("data-image")
+        $.post({
+            url: "/reduce-from-cart",
+            data: {
+                'id': _productId,
+                'name': _productTitle,
+                'price': _productPrice,
+                'image': _productImage,
+                'quantity': _qty
+            },
+            dataType: 'json',
+            complete: function () {
+                location.reload()
+            }
         })
     })
 })
+
